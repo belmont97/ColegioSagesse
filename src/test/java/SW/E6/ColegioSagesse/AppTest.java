742f99aa8d8cba4d48d9ef75efb80e46c4c91120
JorgeDhSS <71732352+JorgeDhSS@users.noreply.github.com>
2020-11-14 00:05:15 -0600

    Add files via upload
    Add basic files and directorys

---
+++ src/test/java/SW/E6/ColegioSagesse/AppTest.java
@@ -0,0 +1,20 @@
+package SW.E6.ColegioSagesse;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
